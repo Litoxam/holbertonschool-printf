470fecf5683adbafe4bdbaa07c5152037deef770
Litoxam <litobernier@gmail.com>
2026-03-25 12:24:13 +0100

    Added the main.h, printf.c and README.md

---
+++ printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+/**
+ * _printf - replicate the original printf
+ *
+ * @format: with specifier.
+ *
+ * Return: number of characters printed, or -1 on error.
+ */
+
+ int _printf(const char *format, ...)
