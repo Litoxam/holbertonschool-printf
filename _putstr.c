a605094676125f41ef35762d8155d93cc24778b6
Litoxam <litobernier@gmail.com>
2026-03-31 22:15:20 +0200

    Pushing the _putstr function, which prints a string and returns the number of char printed

---
+++ _putstr.c
@@ -0,0 +1,21 @@
+/**
+ * _putstr - Prints a string
+ * @s: The string to be printed
+ *
+ * Return: The number of characters printed
+ */
+int _putstr(char *s)
+{
+	int i = 0;
+
+	if (s == NULL)
+		return (0);
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+
+	return (i);
+}
