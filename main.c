1ee9b28a9cf48b38e056fe043972b2b767fca0df
lonie <12333@holbertonstudents.com>
2026-04-02 19:08:46 +0200

    add the main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+#include "_printf.h"
+
+int main(void)
+{
+    int age = 20;
+    char lettre = 'A';
+    float pi = 3.14;
+    char nom = "Lonie";
+
+    _printf("Test simple\n");
+
+    _printf("Entier : %d\n", age);
+    _printf("Caractère : %c\n", lettre);
+    _printf("Flottant : %f\n", pi);
+    _printf("Chaîne : %s\n", nom);
+    _printf("mélange : %d %c %s %f\n", age, lettre, nom, pi);
+
+    printf("Vrai : %d %c %s %f\n", age, lettre, nom, pi);
+    _printf("Moi  : %d %c %s %f\n", age, lettre, nom, pi);
+
+    _printf("String NULL : %s\n", NULL);
+
+    return 0;
+}
