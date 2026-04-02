f2575050c0787104c789a1dacd978878afa1e384
lonie <12333@holbertonstudents.com>
2026-04-02 20:05:12 +0200

    add the test and change the name

--- main.c
+++ main.c
@@ -4,20 +4,17 @@ int main(void)
 {
     int age = 20;
     char lettre = 'A';
-    float pi = 3.14;
-    char nom = "Lonie";
+    double pi = 3.14;
+    char *nom = "Lonie";
 
     _printf("Test simple\n");
-
     _printf("Entier : %d\n", age);
     _printf("Caractère : %c\n", lettre);
     _printf("Flottant : %f\n", pi);
     _printf("Chaîne : %s\n", nom);
     _printf("mélange : %d %c %s %f\n", age, lettre, nom, pi);
-
     printf("Vrai : %d %c %s %f\n", age, lettre, nom, pi);
     _printf("Moi  : %d %c %s %f\n", age, lettre, nom, pi);
-
     _printf("String NULL : %s\n", NULL);
 
     return 0;
