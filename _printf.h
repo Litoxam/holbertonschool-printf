410cd09510700763394876cb7e57f03ea60ee36a
lonie <12333@holbertonstudents.com>
2026-04-02 19:15:10 +0200

    add the .c and .h

---
+++ _printf.h
@@ -0,0 +1,16 @@
+#ifndef _PRINTF_H
+#define _PRINTF_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+
+void print_char(char c);
+void print_string(char *s);
+int print_int(int n);
+void print_float(  f);
+
+char *convert_int_to_string(int )
+
+#endif
