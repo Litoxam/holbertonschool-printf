d74a04c0f67518efd6e285d8cf968f899e818361
Litoxam <litobernier@gmail.com>
2026-04-03 10:46:05 +0200

    Use of the found variable. If there is no match after a % (for example %y), we print the % and the char (L.64)

--- _printf.c
+++ _printf.c
@@ -15,11 +15,13 @@ int _printf(const char *format, ...)
 
 	int i = 0; /*will help us navigating in the format string*/
 	int counter = 0; /*counts the number of char in the string*/
+	int found; /*check if we find a match after the %*/
 
-	specifier array_of_struct[3] = { /*array of struct with specifier char*/
+	specifier array_of_struct[] = { /*array of struct with specifier char*/
 		{'c', print_char}, /*for a char*/
 		{'s', print_string},/*for a string*/
-		{'%', print_percent} /*for a percrent after another one*/
+		{'%', print_percent},/*for a percrent after another one*/
+		{0, NULL}
 	};
 
 	if (format == NULL) /*we check if the ini string is NULL*/
@@ -27,36 +29,47 @@ int _printf(const char *format, ...)
 
 
 	va_start(args, format);
-
+	
 	while (format[i] != '\0') /*we go on the loop until the end*/
 	{
-	if (format[i] != '%') /* if the char is diff than a % */
-	{
-
-		_putchar(format[i]);
-		counter++;
-	}
-
-
-	else  /*if there is a %, we check the char after*/
+		if (format[i] != '%') /* if the char is diff than a % */
+		{	
+			_putchar(format[i]);
+			counter++;
+		} 
+		
+		else  /*if there is a %, we check the char after*/
 	{
 		i++;
 		if (format[i] == '\0') /*if % is the last char of the string*/
+		{
+			va_end(args);	
 			return (-1);
+		}
 
 		j = 0;
+		found = 0;
 		while (j < 3) /* we compare it with our array of struct*/
 		{
 			 /*if %[] == specifier[j]*/
 			if (format[i] == array_of_struct[j].specifier)
 			{
 				counter += array_of_struct[j].f(args);
+				found = 1;
 				break;/*we call the function linked to it*/
 			}
 			j++;
 		}
+		
+		if (found == 0)
+		{
+			_putchar('%');
+			_putchar(format[i]);
+			counter += 2;
+		}
 	}
-
+		
+	
 	i++;
 	}
 	va_end(args);
